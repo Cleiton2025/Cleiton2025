132f69ba0636d313d11842373b144d00a56bbd66
Cleiton2025 <cleitongouvea91@gmail.com>
2025-09-04 20:41:31 -0300

    Add files via upload

---
+++ APP Previsao do Tempo/scripts.js
@@ -0,0 +1,27 @@
+
+
+const key = "a7cb45426a7d0c0ebd52af9165f6e110"
+
+function colocarDadosNaTela(dados) {
+console.log(dados)
+document.querySelector(".cidade").innerHTML = "tempo em " + dados.name
+document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+}
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    colocarDadosNaTela(dados)
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value;
+
+  buscarCidade(cidade)
+}
+
+
+
